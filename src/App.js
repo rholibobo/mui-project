a080242edf9aa9c4781ed17e4fe076ba566f25fa
rholibobo <rholipop@gmail.com>
2023-04-22 00:26:40 +0100

    Created my components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Sidebar from "./components/Sidebar"
+import Feedbar from "./components/Feedbar"
+import Rightbar from "./components/Rightbar"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Sidebar />
+      <Feedbar />
+      <Rightbar />
     </div>
-  );
+  ) ;
 }
 
 export default App;
